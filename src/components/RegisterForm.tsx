02ce6630717524c4f879acf324da19a00162a287
ManojAvhad13 <manojavhad05@gmail.com>
2026-02-02 13:41:16 +0530

    commit

--- src/components/RegisterForm.tsx
+++ src/components/RegisterForm.tsx
@@ -31,7 +31,8 @@ const RegisterForm = ({ previousStep }: propType) => {
                 name, email, password
             })
 
-            console.log(result.data)
+            router.push("/login")
+            // console.log(result.data)
             setLoading(false)
         } catch (error) {
             console.log(error)
@@ -121,13 +122,13 @@ const RegisterForm = ({ previousStep }: propType) => {
                     <span className="flex-1 h-px bg-gray-200"></span>
                 </div>
 
-                <button className="w-full flex items-center justify-center gap-3 border border-gray-300 hover:bg-gray-50 
-                py-3 rounded-xl text-gray-700 font-medium transition-all duration-200" onClick={() => signIn("google")}>
+                <div className="w-full flex items-center justify-center gap-3 border border-gray-300 hover:bg-gray-50 
+                py-3 rounded-xl text-gray-700 font-medium transition-all duration-200" onClick={() => signIn("google", { callbackUrl: "/" })}>
                     <Image src={googleImage} width={20} height={20} alt="googleimg" />
                     Continue with Google
-                </button>
-
+                </div>
             </motion.form>
+
 
             <p className="cursor-pointer font-semibold text-gray-600 mt-6 text-sm flex items-center gap-1" onClick={() => router.push("/login")}>Already have an account ? <LogIn className="w-4 h-4" /><span className="text-green-600">Sign in</span></p>
         </div>
