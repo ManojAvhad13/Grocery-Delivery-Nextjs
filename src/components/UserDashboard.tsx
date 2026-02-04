55da7dd4e9c4bca74ba8d163c560cf5a01005fca
ManojAvhad13 <manojavhad05@gmail.com>
2026-02-04 15:52:06 +0530

    import HeroSection component in UserDashboard

---
+++ src/components/UserDashboard.tsx
@@ -0,0 +1,18 @@
+'use client'
+
+import React, { useEffect, useState } from 'react'
+import HeroSection from './HeroSection'
+import { Leaf, Smartphone, Truck } from 'lucide-react'
+
+const UserDashboard = () => {
+
+
+
+    return (
+        <>
+            <HeroSection />
+        </>
+    )
+}
+
+export default UserDashboard
