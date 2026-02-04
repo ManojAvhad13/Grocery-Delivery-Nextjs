99d9e8ea51cded93db77f7a2b7b5a32f897f49ef
ManojAvhad13 <manojavhad05@gmail.com>
2026-02-04 18:35:56 +0530

    commit

--- src/components/UserDashboard.tsx
+++ src/components/UserDashboard.tsx
@@ -1,12 +1,8 @@
 'use client'
 
-import React, { useEffect, useState } from 'react'
 import HeroSection from './HeroSection'
-import { Leaf, Smartphone, Truck } from 'lucide-react'
 
 const UserDashboard = () => {
-
-
 
     return (
         <>
