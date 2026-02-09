e0b40cbca9d339fbd2bc62899d32ab6bbcc37a13
ManojAvhad13 <manojavhad05@gmail.com>
2026-02-09 20:33:44 +0530

    commit

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,8 @@
 import type { Metadata } from "next";
 import "./globals.css";
 import Provider from "@/Provider";
+import StoreProvider from "@/redux/StoreProvider";
+import InitUser from "@/InitUser";
 
 export const metadata: Metadata = {
   title: "Grocery | 10 minutes grocery delivery",
@@ -16,7 +18,10 @@ export default function RootLayout({
     <html lang="en">
       <body className="w-full min-h-screen bg-linear-to-b from-green-200 to-white">
         <Provider>
-          {children}
+          <StoreProvider>
+            <InitUser />
+            {children}
+          </StoreProvider>
         </Provider>
       </body>
     </html>
