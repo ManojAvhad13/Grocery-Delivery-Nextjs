374fc9d7a67b0f106ca46110604257fbbaa0b476
ManojAvhad13 <manojavhad05@gmail.com>
2026-02-10 10:53:19 +0530

    update link

--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -120,7 +120,7 @@ border border-green-400/60 shadow-lg">
                         <Search className='text-green-600 w-6 h-6' />
                     </div>
 
-                    <Link href={""} className='relative bg-white rounded-full w-11 h-11 flex items-center justify-center shadow-md hover:scale-105 transition'>
+                    <Link href={"/user/cart"} className='relative bg-white rounded-full w-11 h-11 flex items-center justify-center shadow-md hover:scale-105 transition'>
                         <LucideShoppingBasket className='text-green-600 w-6 h-6' />
                         <span className='absolute -top-1 -right-1 bg-red-500 text-white text-xs w-5 h-5 flex items-center justify-center rounded-full font-semibold shadow'>{cartData.length}</span>
                     </Link>
