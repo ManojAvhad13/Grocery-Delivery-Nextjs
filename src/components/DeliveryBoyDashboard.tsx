c68e2c20fbfdd3966fd575858425b81cc6776c52
ManojAvhad13 <manojavhad05@gmail.com>
2026-02-19 19:46:21 +0530

    Implement fetchAssignment function in DeliveryBoyDashboard to retrieve delivery assignments

--- src/components/DeliveryBoyDashboard.tsx
+++ src/components/DeliveryBoyDashboard.tsx
@@ -1,6 +1,22 @@
 'use client'
 
+import axios from "axios"
+import { useEffect } from "react"
+
 const DeliveryBoyDashboard = () => {
+
+    useEffect(() => {
+        const fetchAssignment = async () => {
+            try {
+                const result = await axios.get("/api/delivery/get-assignments")
+                console.log(result)
+            } catch (error) {
+                console.log(error)
+            }
+        }
+        fetchAssignment()
+    }, [])
+
     return (
         <div>
             Delivery Dasboard
