bd238923a2039f6ef01b818d3ecbbb5ad5d3d174
ManojAvhad13 <manojavhad05@gmail.com>
2026-02-21 14:05:21 +0530

    updated register api

--- src/app/api/auth/register/route.ts
+++ src/app/api/auth/register/route.ts
@@ -6,11 +6,20 @@ import { NextRequest, NextResponse } from "next/server";
 export async function POST(req: NextRequest) {
     try {
         await connectDb()
-        const { name, email, password } = await req.json()
+        // const { name, email, password } = await req.json()
+        const body = await req.json()
+        const { name, email, password } = body
+
+        if (!name || !email || !password) {
+            return NextResponse.json(
+                { message: "All fields required" },
+                { status: 400 }
+            )
+        }
 
         // Find user email
 
-        const existUser = await User.findOne({ email })
+        const existUser = await User.findOne({ email: email.toLowerCase() })
         if (existUser) {
             return NextResponse.json(
                 { message: "email already exist!" },
@@ -33,11 +42,16 @@ export async function POST(req: NextRequest) {
 
         // Create User
         const user = await User.create({
-            name, email, password: hashedPassword
+            name,
+            email: email.toLowerCase(),
+            password: hashedPassword
         })
 
         return NextResponse.json(
-            user,
+            {
+                message: "User created",
+                user: { id: user._id, name: user.name, email: user.email }
+            },
             { status: 200 }
         )
 
