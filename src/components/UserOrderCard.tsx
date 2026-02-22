3d4410e8240a4cfff3a5f4488de32e5794720093
ManojAvhad13 <manojavhad05@gmail.com>
2026-02-22 22:00:46 +0530

    updated user order for navigation

--- src/components/UserOrderCard.tsx
+++ src/components/UserOrderCard.tsx
@@ -154,7 +154,7 @@ const UserOrderCard = ({ order }: { order: IOrder }) => {
                 </div>
                     <button className='w-full flex items-center justify-center gap-2 bg-green-600 text-white font-semibold
                 px-4 py-2 rounded-xl shadow hover:bg-green-700 transition'
-                        onClick={() => router.push("/user/track-order")}
+                        onClick={() => router.push(`/user/track-order/${order._id?.toString()}`)}
                     ><Truck size={18} /> Track Your Order</button>
                 </>
                 }
