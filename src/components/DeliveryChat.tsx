5df27ec3f6ba475efc960b1cce73a2d8ad29ef26
ManojAvhad13 <manojavhad05@gmail.com>
2026-02-24 13:35:54 +0530

    intigrate AI Chat suggestions

--- src/components/DeliveryChat.tsx
+++ src/components/DeliveryChat.tsx
@@ -1,7 +1,7 @@
 import { getSocket } from '@/lib/socket'
 import { IMessage } from '@/models/message.model'
 import axios from 'axios'
-import { Send } from 'lucide-react'
+import { Loader2, Send, Sparkles } from 'lucide-react'
 import mongoose from 'mongoose'
 import { motion, AnimatePresence } from 'motion/react'
 import React, { useEffect, useRef, useState } from 'react'
@@ -14,8 +14,9 @@ const DeliveryChat = ({ orderId, deliveryBoyId }: props) => {
 
     const [newMessage, setNewMessage] = useState("")
     const [messages, setMessages] = useState<IMessage[]>()
-
     const chatBoxRef = useRef<HTMLDivElement>(null)
+    const [loading, setLoading] = useState(false)
+    const [suggestions, setSuggestions] = useState([])
 
     useEffect(() => {
         const socket = getSocket()
@@ -70,8 +71,45 @@ const DeliveryChat = ({ orderId, deliveryBoyId }: props) => {
         getAllMessages()
     }, [])
 
+    const getSuggestion = async () => {
+        setLoading(true)
+        try {
+            const lastMessage = messages?.filter(m => m.senderId !== deliveryBoyId)?.at(-1)
+            const result = await axios.post("/api/chat/ai-suggestions", { message: lastMessage?.text, role: "delivery_boy" })
+            // console.log(result.data)
+            setSuggestions(result.data)
+            setLoading(false)
+        } catch (error) {
+            console.log(error)
+            setLoading(false)
+        }
+    }
+
     return (
         <div className='bg-white rounded-3xl shadow-lg border p-4 h-[430] flex flex-col'>
+
+            <div className='flex justify-between items-center mb-3'>
+                <span className='font-semibold text-gray-700 text-sm'>Quick Replies</span>
+                <motion.button
+                    whileTap={{ scale: 0.9 }}
+                    disabled={loading}
+                    onClick={getSuggestion}
+                    className='px-3 py-1 text-xs flex items-center gap-1 bg-purple-100 text-purple-700 rounded-full shadow-sm border border-purple-200 cursor-pointer'
+                ><Sparkles size={14} />{loading ? <Loader2 className='w-5 h-5 animate-spin' /> : "AI suggest"}</motion.button>
+            </div>
+
+            <div className='flex gap-2 flex-wrap mb-3'>
+                {suggestions.map((s, i) => (
+                    <motion.div
+                        key={s}
+                        whileTap={{ scale: 0.92 }}
+                        className='px-3 py-1 text-xs bg-green-50 border border-green-200 text-green-700 rounded-full cursor-pointer'
+                        onClick={() => setNewMessage(s)}
+                    >
+                        {s}
+                    </motion.div>
+                ))}
+            </div>
 
             <div className='flex-1 overflow-y-auto p-2 space-y-3' ref={chatBoxRef}>
                 <AnimatePresence>
