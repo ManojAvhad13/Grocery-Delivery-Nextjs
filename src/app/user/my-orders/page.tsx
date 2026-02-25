ee1d824622c00d41af5c7107e83246f764b2113f
ManojAvhad13 <manojavhad05@gmail.com>
2026-02-25 11:43:48 +0530

    added IOrder interface and updated socket for real time show information for assigned order

--- src/app/user/my-orders/page.tsx
+++ src/app/user/my-orders/page.tsx
@@ -1,6 +1,5 @@
 'use client'
 
-import { IOrder } from '@/models/order.model'
 import axios from 'axios'
 import { ArrowLeft, Package, PackageSearch } from 'lucide-react'
 import { div } from 'motion/react-client'
@@ -8,6 +7,47 @@ import { useRouter } from 'next/navigation'
 import { motion } from 'motion/react'
 import React, { useEffect, useState } from 'react'
 import UserOrderCard from '@/components/UserOrderCard'
+import { getSocket } from '@/lib/socket'
+import mongoose from 'mongoose'
+import { IUser } from '@/models/user.model'
+
+interface IOrder {
+    _id?: mongoose.Types.ObjectId
+    user: mongoose.Types.ObjectId
+    items: [
+        {
+            grocery: mongoose.Types.ObjectId
+            name: string,
+            price: string,
+            unit: string,
+            image: string,
+            quantity: number,
+        }
+    ],
+
+    isPaid: boolean,
+
+    totalAmount: number,
+    paymentMethod: "cod" | "online",
+    address: {
+        fullName: string,
+        mobile: string,
+        city: string,
+        state: string,
+        pincode: string,
+        fullAddress: string,
+        latitude: number,
+        longitude: number,
+    }
+
+    assignment?: mongoose.Types.ObjectId
+    assignedDeliveryBoy?: IUser
+
+    status: "pending" | "out of delivery" | "delivered",
+
+    createdAt?: Date
+    updatedAt?: Date
+}
 
 const page = () => {
 
@@ -28,6 +68,16 @@ const page = () => {
             }
         }
         getMyOrders()
+    }, [])
+
+    useEffect(() => {
+        const socket = getSocket()
+        socket.on("order-assigned", ({ orderId, assignedDeliveryBoy }) => {
+            setOrders((prev) => prev?.map((o) => (
+                o._id == orderId ? { ...o, assignedDeliveryBoy } : o
+            )))
+        })
+        return () => { socket.off("order-assigned") }
     }, [])
 
     if (loading) {
