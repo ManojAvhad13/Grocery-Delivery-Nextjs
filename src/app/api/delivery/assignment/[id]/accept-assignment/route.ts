27d6c29f354c8875e2c012f6910a05813b612757
ManojAvhad13 <manojavhad05@gmail.com>
2026-02-25 11:42:25 +0530

    added new emitEventHandler for order-assigned

--- src/app/api/delivery/assignment/[id]/accept-assignment/route.ts
+++ src/app/api/delivery/assignment/[id]/accept-assignment/route.ts
@@ -1,5 +1,6 @@
 import { auth } from "@/auth";
 import connectDb from "@/lib/db";
+import emitEventHandler from "@/lib/emitEventHandler";
 import DeliveryAssignment from "@/models/deliveryAssignment.model";
 import Order from "@/models/order.model";
 import { NextRequest, NextResponse } from "next/server";
@@ -47,6 +48,10 @@ export async function GET(req: NextRequest, { params }: { params: { id: string }
         }
         order.assignedDeliveryBoy = deliveryBoyId
         await order.save()
+
+        await order.populate("assignedDeliveryBoy")
+
+        await emitEventHandler("order-assigned", { orderId: order._id, assignedDeliveryBoy: order.assignedDeliveryBoy })
 
         await DeliveryAssignment.updateMany(
             {
