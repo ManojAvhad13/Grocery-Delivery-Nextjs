22402553a0e53466c00b8db5fcd6bed6d3ed884d
ManojAvhad13 <manojavhad05@gmail.com>
2026-02-26 22:58:58 +0530

    updated view grocery edit functionality completed

--- src/app/admin/view-grocery/page.tsx
+++ src/app/admin/view-grocery/page.tsx
@@ -3,16 +3,37 @@
 import axios from 'axios'
 import React, { useEffect, useState } from 'react'
 import { AnimatePresence, motion, scale } from "motion/react"
-import { ArrowLeft, Edit, Package, Search } from 'lucide-react'
+import { ArrowLeft, Edit, Loader2, Package, Search, Trash2, Upload, X } from 'lucide-react'
 import { useRouter } from 'next/navigation'
 import { IGrocery } from '@/models/grocery.model'
 import Image from 'next/image'
+
+const categories = [
+    "Fruits & Vegetables",
+    "Dairy & Eggs",
+    "Rice, Atta & Grains",
+    "Snacks & Biscuits",
+    "Spices & Masalas",
+    "Beverages & Drinks",
+    "Personal Care",
+    "Household Essentials",
+    "Instant & Packaged Food",
+    "Baby & Pet Care",
+]
+
+const units = [
+    "kg", "g", "liter", "ml", "piece", "pack"
+]
 
 const ViewGrocery = () => {
 
     const router = useRouter()
     const [groceries, setGroceries] = useState<IGrocery[]>()
     const [editing, setEditing] = useState<IGrocery | null>(null)
+    const [imagePreview, setImagePreview] = useState<string | null>(null)
+    const [backendImage, setBackendImage] = useState<Blob | null>(null)
+    const [loading, setLoading] = useState(false)
+    const [deleteLoading, setDeleteLoading] = useState(false)
 
     useEffect(() => {
         const getGroceries = async () => {
@@ -25,6 +46,60 @@ const ViewGrocery = () => {
         }
         getGroceries()
     }, [])
+
+    useEffect(() => {
+        if (editing) {
+            setImagePreview(editing.image)
+        }
+    }, [editing])
+
+    const handleImageChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+        const file = e.target.files?.[0]
+        if (file) {
+            setBackendImage(file)
+            setImagePreview(URL.createObjectURL(file))
+        }
+    }
+
+    const handleEdit = async () => {
+        setLoading(true)
+        if (!editing) return
+
+        try {
+            const formData = new FormData()
+            formData.append("groceryId", editing?._id?.toString()!)
+            formData.append("name", editing.name)
+            formData.append("category", editing.category)
+            formData.append("price", editing.price.toString())
+            formData.append("unit", editing.unit)
+            if (backendImage) {
+                formData.append("image", backendImage)
+            }
+            const result = await axios.post("/api/admin/edit-groceries", formData)
+            // console.log(result)
+            setLoading(false)
+            window.location.reload()
+        } catch (error) {
+            console.log(error)
+            setLoading(false)
+        }
+    }
+
+    const handleDelete = async () => {
+
+        setDeleteLoading(true)
+
+        if (!editing) return
+
+        try {
+            const result = await axios.post("/api/admin/delete-grocery", { groceryId: editing._id })
+            setDeleteLoading(false)
+            window.location.reload()
+        } catch (error) {
+            console.log(error)
+            setDeleteLoading(false)
+        }
+    }
 
     return (
         <div className='pt-4 w-[95%] md:w-[85%] mx-auto pb-20'>
@@ -91,7 +166,99 @@ const ViewGrocery = () => {
             </div>
 
             <AnimatePresence>
+                {editing && (
+                    <motion.div
+                        initial={{ opacity: 0 }}
+                        animate={{ opacity: 1 }}
+                        exit={{ opacity: 0 }}
 
+                        className='fixed inset-0 bg-black/40 flex items-center justify-center z-50 backdrop-blur-sm px-4'
+                    >
+
+                        <motion.div
+                            initial={{ opacity: 0, y: 40 }}
+                            animate={{ opacity: 1, y: 0 }}
+                            exit={{ opacity: 0, y: 40 }}
+                            transition={{ duration: 0.3 }}
+
+                            className='bg-white rounded-2xl shadow-2xl w-full max-w-md p-7 relative'
+                        >
+                            <div className='flex justify-between items-center mb-4'>
+                                <h2 className='text-2xl font-bold text-green-700'
+                                >Edit Grocery</h2>
+                                <button className='text-gray-600 hover:text-red-600'
+                                    onClick={() => setEditing(null)}>
+                                    <X size={18} />
+                                </button>
+                            </div>
+
+                            <div className='relative aspect-square w-36 rounded-lg overflow-hidden mb-4 border border-gray-200 group'>
+                                {imagePreview && <Image src={imagePreview} alt={editing.name} fill
+                                    className='object-cover' />}
+                                <label htmlFor='imageUpload'
+                                    className='absolute inset-0 bg-black/40 opacity-0 group-hover:opacity-95 flex items-center justify-center cursor-pointer transition-opacity'>
+                                    <Upload size={28} className='text-white' /> <span className='text-white'>Upload Image</span></label>
+                                <input type="file" id='imageUpload' accept='image/*' hidden onChange={handleImageChange} />
+                            </div>
+
+                            <div className='space-y-4'>
+                                <input type="text"
+                                    placeholder='Enter Grocery Name...'
+                                    value={editing.name}
+                                    onChange={(e) => setEditing({ ...editing, name: e.target.value })}
+                                    className='w-full border border-gray-300 rounded-lg p-2.5 focus:ring-2 
+                                    focus:ring-green-500 outline-none' />
+
+                                <select value={editing.category}
+                                    onChange={(e) => setEditing({ ...editing, category: e.target.value })}
+                                    className='w-full border border-gray-300 rounded-lg p-2.5 focus:ring-2 focus:ring-green-500 
+                                outline-none bg-white'>
+                                    <option>Select Category</option>
+                                    {categories.map((c, i) => (
+                                        <option key={i} value={c}>{c}</option>
+                                    ))}
+                                </select>
+
+                                <input type="text"
+                                    placeholder='Price'
+                                    value={editing.price}
+                                    onChange={(e) => setEditing({ ...editing, price: e.target.value })}
+                                    className='w-full border border-gray-300 rounded-lg p-2.5 focus:ring-2 
+                                    focus:ring-green-500 outline-none' />
+
+                                <select value={editing.unit}
+                                    onChange={(e) => setEditing({ ...editing, unit: e.target.value })}
+                                    className='w-full border border-gray-300 rounded-lg p-2.5 focus:ring-2 focus:ring-green-500 
+                                outline-none bg-white'>
+                                    <option>Select Category</option>
+                                    {units.map((u, i) => (
+                                        <option key={i} value={u}>{u}</option>
+                                    ))}
+                                </select>
+                            </div>
+
+                            <div className='flex justify-end gap-3 mt-6'>
+                                <button type='button' className='px-4 py-2 rounded-lg bg-green-600 text-white flex items-center gap-2 
+                                hover:bg-green-700 transition-all'
+                                    onClick={handleEdit}
+                                    disabled={loading}
+                                >
+                                    {loading ? <Loader2 size={14} /> : "Edit Grocery"}
+                                </button>
+
+                                <button className='px-4 py-2 rounded-lg bg-red-600 text-white flex items-center gap-2 
+                                hover:bg-red-700  transition'
+                                    onClick={handleDelete}
+                                    disabled={deleteLoading}
+                                >
+                                    {deleteLoading ? <Loader2 size={14} /> : "Delete Grocery"}
+                                </button>
+                            </div>
+
+                        </motion.div>
+
+                    </motion.div>
+                )}
             </AnimatePresence>
 
         </div>
