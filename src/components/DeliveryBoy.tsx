936d1c79ec5c94a20339225a47de4239ae7abb39
ManojAvhad13 <manojavhad05@gmail.com>
2026-02-27 10:59:21 +0530

    Updated for earning

--- src/components/DeliveryBoy.tsx
+++ src/components/DeliveryBoy.tsx
@@ -1,10 +1,26 @@
 import React from 'react'
 import DeliveryBoyDashboard from './DeliveryBoyDashboard'
+import { auth } from '@/auth'
+import connectDb from '@/lib/db'
+import Order from '@/models/order.model'
 
-const DeliveryBoy = () => {
+const DeliveryBoy = async () => {
+
+    await connectDb()
+    const session = await auth()
+    const DeliveryBoyId = session?.user?.id
+    const orders = await Order.find({
+        assignedDeliveryBoy: DeliveryBoyId,
+        deliveryOtpVerification: true
+    })
+
+    const today = new Date().toDateString()
+    const todayOrders = orders.filter((o) => new Date(o.deliveredAt).toString() === today).length
+    const todayEarning = todayOrders * 40
+
     return (
         <>
-            <DeliveryBoyDashboard />
+            <DeliveryBoyDashboard earning={todayEarning} />
         </>
     )
 }
